075b8b8da1362e0dc00c4cafc76f2d621dd95cf1
Asiyanaveed <160754902+Asiyanaveed@users.noreply.github.com>
2024-03-01 17:28:04 +0500

    Add files via upload

---
+++ 12th.js
@@ -0,0 +1,5 @@
+var member = ['Asiya', 'Naveed', 'Warisha', 'Abdul Ahad'];
+var message = 'kal kya date he?';
+for (var i = 0; i < member.length; i++) {
+    console.log(message + member[i]);
+}
---
+++ 12th.ts
@@ -0,0 +1,5 @@
+let member: string[]=['Asiya','Naveed','Warisha','Abdul Ahad'];
+let message: string='kal kya date he?';
+for (let i=0; i<member.length;i++){
+    console.log(message+member[i]);
+}
